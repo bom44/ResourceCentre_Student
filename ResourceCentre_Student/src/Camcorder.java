716939fa2f9c1fb1bbc6a7110ece876011d7a972
ymlee <ymlee@172.19.0.100>
2021-07-27 15:47:23 +0800

    add new private attribute for number of lenses

--- ResourceCentre_Student/src/Camcorder.java
+++ ResourceCentre_Student/src/Camcorder.java
@@ -2,6 +2,7 @@
 public class Camcorder extends Item{
 	private int opticalZoom;
 	private int pan;
+	private int numberOfLenses;
 
 	public Camcorder(String assetTag, String description, int opticalZoom) {
 		super(assetTag, description);
